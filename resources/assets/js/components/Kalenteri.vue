c6e6e7c3b05be80b0b2c50bf6a12e6c6e967b6c9
Anssi Hautaviita <anssi@Anssis-MacBook-Pro.local>
2018-02-12 00:17:43 +0200

    Progress with calendar functionality

--- resources/assets/js/components/Kalenteri.vue
+++ resources/assets/js/components/Kalenteri.vue
@@ -13,59 +13,59 @@
                 <div class="day-of-the-week">LA</div>
                 <div class="day-of-the-week">SU</div>
             </div>
-            <div class="week">
-                <div class="day empty"></div>
-                <div class="day empty"></div>
-                <div class="day empty"></div>
-                <div class="day">1</div>
-                <div class="day">2</div>
-                <div class="day saturday">3</div>
-                <div class="day active sunday">4</div>
-            </div>
-            <div class="week">
-                <div class="day">5</div>
-                <div class="day">6</div>
-                <div class="day">7</div>
-                <div class="day">8</div>
-                <div class="day">9</div>
-                <div class="day saturday">10</div>
-                <div class="day sunday">11</div>
-            </div>
-            <div class="week">
-                <div class="day">12</div>
-                <div class="day">13</div>
-                <div class="day">14</div>
-                <div class="day">15</div>
-                <div class="day">16</div>
-                <div class="day saturday">17</div>
-                <div class="day sunday">18</div>
-            </div>
-            <div class="week">
-                <div class="day">19</div>
-                <div class="day">20</div>
-                <div class="day">21</div>
-                <div class="day">22</div>
-                <div class="day">23</div>
-                <div class="day saturday">24</div>
-                <div class="day sunday">25</div>
-            </div>
-            <div class="week">
-                <div class="day">26</div>
-                <div class="day">27</div>
-                <div class="day">28</div>
-                <div class="day empty"></div>
-                <div class="day empty"></div>
-                <div class="day empty saturday"></div>
-                <div class="day empty sunday"></div>
-            </div>
+            <paiva 
+                v-for="n in emptyDaysBeforeStart" 
+                :key="n" 
+                day-number=0
+            />
+            <paiva 
+                v-for="n in daysInSelectedMonth" 
+                :key="n+100" 
+                :day-number="n"
+                :is-selected-date="n == selectedDay"
+                @click.native="selectedDay = n"
+            />
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Calendar mounted.')
+
+import päivä from "./subcomponents/päivä";
+
+function emptyDaysBeforeStart(month, year) {
+    var day = moment(new Date(year, month, 1));
+    var weekday = day.isoWeekday();
+    return weekday - 1;
+}
+
+var currentDate = moment(),
+    selectedDate = currentDate,
+    daysInSelectedMonth = selectedDate.daysInMonth()
+    emptyDaysBeforeStart = emptyDaysBeforeStart(selectedDate.get("month"), selectedDate.get("year"));
+
+export default {
+    components: {
+        'paiva': päivä
+    },
+    semiRandomKey: function() {
+        return Math.ceil(Math.random()*99999999); // I know this is dirty. I am sorry.
+    },
+    clicked: function() {
+        console.log("clicked");
+    },
+    data: function() {
+        return {
+            currentDate,
+            selectedDate,
+            daysInSelectedMonth: selectedDate.daysInMonth(),    
+            emptyDaysBeforeStart,
+            "selectedDay": selectedDate.get("date")
         }
+    },
+    mounted() {
+        console.log();
     }
+}
+
 </script>
---
+++ resources/assets/js/components/subcomponents/Päivä.vue
@@ -0,0 +1,17 @@
+<template>
+    <div 
+        class="day" 
+        v-bind:class="{ empty: !dayNumber, active: isSelectedDate }"
+    >
+        {{ dayNumber == 0 ? "&nbsp;" : dayNumber }}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["dayNumber", "weekday", "isSelectedDate"],
+        mounted() {
+            
+        }
+    }
+</script>
